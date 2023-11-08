e7cfe77f183fe3718c2582f5c44e1621f333b5d4
Kevin <kwcw@live.com>
2023-11-08 20:53:33 +1100

    Forms in admin area

--- OrchardCoreBackEnd/obj/Debug/net6.0/OrchardCoreBackEnd.MvcApplicationPartsAssemblyInfo.cs
+++ OrchardCoreBackEnd/obj/Debug/net6.0/OrchardCoreBackEnd.MvcApplicationPartsAssemblyInfo.cs
@@ -163,6 +163,7 @@ using System.Reflection;
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("OrchardCore.XmlRpc")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("SafeMode")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("SixLabors.ImageSharp.Web")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("TestModule")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("TheAdmin")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("TheAgencyTheme")]
