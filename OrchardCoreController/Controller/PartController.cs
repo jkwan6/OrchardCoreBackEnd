563bda18e38e174df7a988f6d68698fa8985208e
Kevin <kwcw@live.com>
2023-11-16 22:06:28 +1100

    Managed to add existing content item to a list.  Just had to update the content item in the _contentMAnager

--- OrchardCoreController/Controller/PartController.cs
+++ OrchardCoreController/Controller/PartController.cs
@@ -4,6 +4,7 @@ using OrchardCore.ContentFields.Fields;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Handlers;
 using OrchardCore.ContentManagement.Routing;
+using OrchardCore.Lists.GraphQL;
 using OrchardCore.Lists.Models;
 using OrchardCore.Markdown.Models;
 using OrchardCore.Title.Models;
@@ -17,6 +18,7 @@ namespace OrchardCoreController.Controller
     public class PartController : ControllerBase
     {
         private readonly IContentManager _contentManager;
+        
         public PartController(IContentManager contentManager)
         {
             _contentManager = contentManager;
@@ -70,18 +72,64 @@ namespace OrchardCoreController.Controller
 
             //var test = await _contentManager.GetAsync("4x6ay4kqv774k4xv7fp5tc91dh");
 
+            var contentItemParent1 = await _contentManager.GetAsync("44xe4hqzxnb8wtqsrzsp99zhmd", VersionOptions.Latest);
+
             var contentItemParent = await _contentManager.GetAsync("4v2gb31g5wjhczzsdh4w00dkwq", VersionOptions.Latest);
             var contentItemChild = await _contentManager.GetAsync("4z0afz604nwe227nca05med9cq", VersionOptions.Latest);
 
-            var listPart = contentItemParent.As<ListPart>();
+            contentItemChild.Weld<ContainedPart>();
+            await contentItemChild.AlterAsync<ContainedPart>(async t => {
+                t.ListContentItemId = "4v2gb31g5wjhczzsdh4w00dkwq";
+                t.ListContentType = "Blog";
+            });
 
+            await _contentManager.UpdateAsync(contentItemChild);
+
+
+
+            //var contentItemListList = contentItemList.As<ListPart>();
+
+            //var contentItem = await _contentManager.NewAsync("BlogPost");
+            //var titlePart = contentItem.As<TitlePart>();
+            //var autoRoutePart = contentItem.As<AutoroutePart>();
+            //var markdownBody = contentItem.As<MarkdownBodyPart>();
+
+            //titlePart.Title = "TestBlogPost";
+            //titlePart.Apply();
+
+            //markdownBody.Markdown = "test";
+            //markdownBody.Apply();
+
+            //var result = await _contentManager.UpdateValidateAndCreateAsync(contentItem, VersionOptions.Draft);
+
+            //await _contentManager.CreateContentItemVersionAsync(contentItem);
+            //await _contentManager.CreateAsync(contentItem, VersionOptions.Draft);
+            //await _contentManager.SaveDraftAsync(contentItem);
+            //await _contentManager.PublishAsync(contentItem);
+            return Ok();
+
+        }
+
+        public async Task<IActionResult> test3()
+        {
+
+            //var test = await _contentManager.GetAsync("4x6ay4kqv774k4xv7fp5tc91dh");
+
+            var contentItemParent1 = await _contentManager.GetAsync("4gpxqvkvbqj4w3agagw6zedzns", VersionOptions.Latest);
+
+            var contentItemParent = await _contentManager.GetAsync("4h4bnc2kgsd16r13ww07t9bent", VersionOptions.Latest);
+            var contentItemChild = await _contentManager.GetAsync("4khhpavv843wf639rkw36rrr48", VersionOptions.Latest);
 
             contentItemChild.Weld<ContainedPart>();
-            await contentItemChild.AlterAsync<ContainedPart>(async x =>
-            {
-                x.ListContentItemId = "4v2gb31g5wjhczzsdh4w00dkwq";
-                x.ListContentType = "Blog";
+            await contentItemChild.AlterAsync<ContainedPart>(async t => {
+                t.ListContentItemId = "4h4bnc2kgsd16r13ww07t9bent";
+                t.ListContentType = "test";
             });
+
+            var containedPart = contentItemChild.As<ContainedPart>();
+            containedPart.ListContentItemId = "4h4bnc2kgsd16r13ww07t9bent";
+            containedPart.Apply();
+
 
             //var contentItemListList = contentItemList.As<ListPart>();
 
